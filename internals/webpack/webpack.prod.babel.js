34ea0448774ee1986ceb1d43970d97bdd020d547
Abdallah Hemedah <Hemedah94@Gmail.com>
2019-10-16 12:43:27 +0200

    fix webpack

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -116,9 +116,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'ABZO',
+      short_name: 'ABZO',
+      description: 'My portfolio!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
